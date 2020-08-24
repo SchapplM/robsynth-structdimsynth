771bdb2635b6d257faff7607ae276a51de482b9b
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2020-08-24 18:26:07 +0100

    [struct_par] Log-Ausgaben für Sperrschutz der ParRobLib

--- struktsynth_par/parroblib_add_robots_symact.m
+++ struktsynth_par/parroblib_add_robots_symact.m
@@ -84,14 +84,14 @@ for iFG = settings.EE_FG_Nr % Schleife über EE-FG (der PKM)
   EE_FG = EE_FG_ges(iFG,:);
   EE_FG_Name = sprintf( '%dT%dR', sum(EE_FG(1:3)), sum(EE_FG(4:6)) );
   % Pfad mit vollständigen Ergebnissen der Struktursynthese
-  parroblib_writelock('lock', 'csv', logical(EE_FG), 600); % nicht lesen, wenn gleichzeitig geschrieben.
+  parroblib_writelock('lock', 'csv', logical(EE_FG), 600, true); % nicht lesen, wenn gleichzeitig geschrieben.
   synthrestable = readtable( ...
     fullfile(parroblibpath,'synthesis_result_lists',[EE_FG_Name,'.csv']), ...
     'ReadVariableNames', true);
   synthrestable_var = readtable( ...
     fullfile(parroblibpath,'synthesis_result_lists',[EE_FG_Name,'_var.csv']), ...
     'ReadVariableNames', true);
-  parroblib_writelock('free', 'csv', logical(EE_FG));
+  parroblib_writelock('free', 'csv', logical(EE_FG), 0, true);
   if ~isempty(synthrestable_var)
     synthrestable = [synthrestable; synthrestable_var]; %#ok<AGROW>
   end
@@ -196,7 +196,7 @@ for iFG = settings.EE_FG_Nr % Schleife über EE-FG (der PKM)
     
     Whitelist_PKM = {};
     tlm_iFKloop = tic(); % zur Speicherung des Zeitpunkts der letzten Meldung
-    parroblib_writelock('lock', 'csv', logical(EE_FG), 30*60); % Sperre csv-Dateien während des Hinzufügens
+    parroblib_writelock('lock', 'csv', logical(EE_FG), 30*60, true); % Sperre csv-Dateien während des Hinzufügens
     for iFK = II' % Schleife über serielle Führungsketten
       ii_kin = ii_kin + 1;
       SName = l.Names_Ndof{iFK};
@@ -314,7 +314,7 @@ for iFG = settings.EE_FG_Nr % Schleife über EE-FG (der PKM)
         Whitelist_PKM = [Whitelist_PKM;{Name}]; %#ok<AGROW>
       end
     end
-    parroblib_writelock('free', 'csv', logical(EE_FG));
+    parroblib_writelock('free', 'csv', logical(EE_FG), 0, true);
     if isempty(Whitelist_PKM)
       fprintf('Für FG %s und G%dP%d gibt es keine PKM.\n', EE_FG_Name, Coupling(1), Coupling(2));
       continue
@@ -383,7 +383,7 @@ for iFG = settings.EE_FG_Nr % Schleife über EE-FG (der PKM)
     if ~settings.offline
       % Erzeuge alle Template-Dateien neu (ohne Kompilierung). Dadurch wird
       % sichergestellt, dass sie die richtige Version haben.
-      parroblib_writelock('lock', 'mex', logical(EE_FG));
+      parroblib_writelock('lock', 'mex', logical(EE_FG), 60*60, true);
       parroblib_create_template_functions(Whitelist_Kin,false,false);
       % Benötigte Funktionen kompilieren (nur wenn Struktursynthese
       % parallel). Bei serieller Struktursynthese wird dort kompiliert.
@@ -396,7 +396,7 @@ for iFG = settings.EE_FG_Nr % Schleife über EE-FG (der PKM)
           RP.fill_fcn_handles(true, true);
         end
       end
-      parroblib_writelock('free', 'mex', logical(EE_FG));
+      parroblib_writelock('free', 'mex', logical(EE_FG), 0, true);
     end
     %% Maßsynthese für Liste von Robotern durchführen
     % Mit dem dann eindeutigen Robotermodell sind weitere Berechnungen
@@ -471,7 +471,7 @@ for iFG = settings.EE_FG_Nr % Schleife über EE-FG (der PKM)
     Structures_Names = unique(Structures_Names); % Eindeutige Liste der Strukturen erzeugen
     fprintf('Verarbeite die %d Ergebnisse der Struktursynthese (%d PKM)\n', ...
       length(Ergebnisliste), length(Structures_Names));
-    parroblib_writelock('lock', 'csv', logical(EE_FG), 30*60); % Sperre beim Ändern der csv
+    parroblib_writelock('lock', 'csv', logical(EE_FG), 30*60, true); % Sperre beim Ändern der csv
     for jjj = 1:length(Structures_Names) % Alle eindeutigen Strukturen durchgehen
       %% Ergebnisse für diese PKM laden
       Name = Structures_Names{jjj};
@@ -581,7 +581,7 @@ for iFG = settings.EE_FG_Nr % Schleife über EE-FG (der PKM)
         end
       end
     end
-    parroblib_writelock('free', 'csv', logical(EE_FG));
+    parroblib_writelock('free', 'csv', logical(EE_FG), 0, true);
     fprintf('Fertig mit PKM-Kinematik %s\n', PName);
   end % Koppelpunkte (Variable kk)
 end % EE-FG (Variable iFG)
