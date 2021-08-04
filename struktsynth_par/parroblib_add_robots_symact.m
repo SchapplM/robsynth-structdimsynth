f52c2bdfba8ec86918fce8055ea31dec81c06b6f
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2021-08-04 22:41:16 +0200

    [struct_par] Falschen Variablennamen korrigiert

--- struktsynth_par/parroblib_add_robots_symact.m
+++ struktsynth_par/parroblib_add_robots_symact.m
@@ -29,7 +29,7 @@ settings_default = struct( ...
   'selectgeneral', true, ... % Auch allgemeine Modelle wählen
   'selectvariants', true, ... % Auch alle Varianten wählen
   'ignore_check_leg_dof', false, ... % Plausibilitätsregeln aus parrob_structsynth_check_leg_dof können ignoriert werden
-  'allow_passive_revolute', false, ... % Technisch sinnvoll. Zum Testen auf true setzen (z.B. für 2T0R und 2T1R PKM)
+  'allow_passive_prismatic', false, ... % Technisch sinnvoll. Zum Testen auf true setzen (z.B. für 2T0R und 2T1R PKM)
   'comp_cluster', false, ... % Rechne auf PBS-Rechen-Cluster. Parallel-Instanz für G-/P-Kombis
   'clustercomp_if_res_olderthan', 2, ... % Falls in den letzten zwei Tagen bereits ein vollständiger Durchlauf gemacht wurde, dann nicht nochmal auf dem Cluster rechnen. Deaktivieren durch Null-Setzen
   'isoncluster', false, ... % Marker um festzustellen, dass gerade auf Cluster parallel gerechnet wird
@@ -268,7 +268,7 @@ for iFG = settings.EE_FG_Nr % Schleife über EE-FG (der PKM)
         end
       end
 
-      if sum(SName=='P')>1 && ~settings.allow_passive_revolute
+      if sum(SName=='P')>1 && ~settings.allow_passive_prismatic
         % Hat mehr als ein Schubgelenk. Kommt nicht für PKM in Frage.
         % (es muss dann zwangsläufig ein Schubgelenk passiv sein)
         parroblib_update_csv({SName}, Coupling, logical(EE_FG), 1, 0);
@@ -327,7 +327,7 @@ for iFG = settings.EE_FG_Nr % Schleife über EE-FG (der PKM)
         if ii < settings.lfdNr_min, continue; end % Starte erst später
         % Prüfe schon hier auf passive Schubgelenke (weniger Rechenaufwand)
         IdxP = (SName(3:3+N_LegDoF-1)=='P'); % Nummer des Schubgelenks finden
-        if ~settings.allow_passive_revolute && any(IdxP) && find(IdxP)~=jj
+        if ~settings.allow_passive_prismatic && any(IdxP) && find(IdxP)~=jj
           continue % Es gibt ein Schubgelenk und es ist nicht das aktuierte Gelenk
         end
         % Prüfe, ob ein Teil eines technischen Gelenks (Kardan, Kugel
