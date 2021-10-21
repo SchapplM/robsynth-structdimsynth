9251e39eb8cf3e42cdee65743ba31e39b5a7af71
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2021-10-21 23:04:47 +0200

    [cds] Skript zur Reproduzierbarkeit erweitert

--- dimsynth/misc/cds_check_results_reproducability.m
+++ dimsynth/misc/cds_check_results_reproducability.m
@@ -1,6 +1,7 @@
 % Prüfe die Reproduzierbarkeit aller Zwischenergebnisse einer Maßsynthese
 % Wenn keine Programmfehler vorliegen, müssten sich alle Fitness-Werte
-% exakt reproduzieren lassen.
+% exakt reproduzieren lassen. Ausnahmen können durch Zufallszahlen und
+% Abweichungen der Algorithmen bei verschiedenen CPU-Architekturen bestehen
 % 
 % Eingabe:
 % OptName
@@ -9,11 +10,14 @@
 % RobName (optional)
 %   Name des Roboters, für den die Ergebnisse nachgerechnet werden sollen.
 %   Auch mehrere Roboter (als 1xN Cell-array) möglich.
+% s_in (optional)
+%   Einstellungen zur Durchführung der Reproduzierbarkeitsstudie.
+%   Felder: Siehe Quelltext
 
 % Moritz Schappler, moritz.schappler@imes.uni-hannover.de, 2020-12
 % (C) Institut für Mechatronische Systeme, Leibniz Universität Hannover
 
-function cds_check_results_reproducability(OptName, RobName)
+function cds_check_results_reproducability(OptName, RobName, s_in)
 
 %% Optimierung laden
 resdir = fullfile(fileparts(which('structgeomsynth_path_init.m')), 'results');
@@ -43,11 +47,27 @@ elseif isa(RobName, 'cell')
 else
   RobNames = {RobName};
 end
+s = struct( ...
+  'only_from_pareto_front', true); % bei false werden alle Partikel geprüft, bei true nur die besten
+if nargin < 3
+  s_in = s;
+end
+for f = fields(s_in)'
+  if isfield(s, f{1})
+    s.(f{1}) = s_in.(f{1});
+  else
+    error('Feld "%s" aus s_in kann nicht übergeben werden', f{1});
+  end
+end
+  
 RobNames = unique(RobNames);
-ReproStatsTab = cell2table(cell(0,7), 'VariableNames', ...
-  {'RobNr', 'Name', 'Partikel', 'ResOpt', 'ResRepro', 'ErrMax', 'Status'});
+ReproStatsTab_empty = cell2table(cell(0,9), 'VariableNames', ...
+  {'RobNr', 'Name', 'Partikel', 'ResOpt', 'ResRepro', 'ErrMax_Rel', ...
+  'ErrMax_Rel_q0set', 'WithDetails', 'Status'});
+ReproStatsTab = ReproStatsTab_empty;
 %% Roboter auswerten und Nachrechnen der Fitness-Funktion
 for i = 1:length(RobNames)
+  ReproStatsTab_Rob = ReproStatsTab_empty;
   RobName = RobNames{i};
   RobNr = find(strcmp(RobName,Structures_Names),1,'first');
   fprintf('Untersuche Reproduzierbarkeit für Rob %d/%d: %s\n', i, length(RobNames), RobName);
@@ -69,7 +89,7 @@ for i = 1:length(RobNames)
     PSO_Detail_Data = [];
   end
   %% Fitness-Wert nachrechnen
-  if ~isempty(PSO_Detail_Data)
+  if ~isempty(PSO_Detail_Data) && ~s.only_from_pareto_front
     % Detail-Ergebnisse verfügbar. Rechne alle Partikel nach.
     ngen = size(PSO_Detail_Data.pval,3);
     nind = size(PSO_Detail_Data.pval,1);
@@ -94,9 +114,11 @@ for i = 1:length(RobNames)
       fval_all = RobotOptRes.fval;
       p_desopt_all = RobotOptRes.desopt_pval(:)';
     end
-    % Variable PSO_Detail_Data nachträglich erzeugen
-    PSO_Detail_Data = struct('fval', NaN(size(fval_all,1),size(fval_all,2),2));
-    PSO_Detail_Data.fval(:,:,1) = fval_all;
+    if isempty(PSO_Detail_Data)
+      % Variable PSO_Detail_Data nachträglich erzeugen
+      PSO_Detail_Data = struct('fval', NaN(size(fval_all,1),size(fval_all,2),2));
+      PSO_Detail_Data.fval(:,:,1) = fval_all;
+    end
   end
   % Debug: Zusätzliche Bilder
   % Set.general.plot_details_in_fitness = 1e10;
@@ -122,26 +144,67 @@ for i = 1:length(RobNames)
     end
     % TODO: Berücksichtige Structure_jj.q0_traj, falls nicht reproduzierbar.
     [k_gen, k_ind] = cds_load_particle_details(PSO_Detail_Data, f_jj);
+    if ~isempty(PSO_Detail_Data) && isfield(PSO_Detail_Data, 'q0_ik')
+      q0 = PSO_Detail_Data.q0_ik(k_ind,:,k_gen)';
+    else
+      q0 = [];
+    end
     fprintf('Reproduktion Partikel Nr. %d (Gen. %d, Ind. %d):\n', jj, k_gen, k_ind);
     f2_jj = cds_fitness(R,Set,Traj,Structure_jj,p_jj,p_desopt_jj);
-    test_f = f_jj - f2_jj;
-    rescode = 0;
-    if any(abs(test_f) > 1e-4) && f_jj(1)<1e3 || ... % Bei Erfolg feine Toleranz
-        any(abs(test_f) > 1e-1) && f_jj(1)>1e3 % Bei ZB-Verletzung gröbere Toleranz
-      warning(['Fitness-Wert zu Partikel Nr. %d (Gen. %d, Ind. %d) nicht ', ...
-        'reproduzierbar. In Optimierung (%s): [%s]. Neu: [%s]. Diff.: [%s]'], ...
+    test_f2_abs = f_jj - f2_jj;
+    test_f2_rel = test_f2_abs ./ f_jj;
+    test_f3_rel = NaN(size(test_f2_rel)); % Initialisierung
+    rescode = 0; % Kein Fehler
+    if any(abs(test_f2_rel) > 1e-2) % Fehler 1%
+      warning(['Fitness-Wert zu Partikel Nr. %d (Gen. %d, Ind. %d) nicht ohne q0 ', ...
+        'reproduzierbar. In Optimierung (%s): [%s]. Neu: [%s]. Diff.: [%s]%%'], ...
         jj, k_gen, k_ind, disp_array(Set.optimization.objective), ...
-        disp_array(f_jj', '%1.4f'), disp_array(f2_jj', '%1.4f'), ...
-        disp_array(test_f', '%1.4e'));
-      rescode = 1;
-      if all(abs(f_jj) < 1e3) && any(abs(f2_jj) > 1e3)
-        rescode = 2;
-        warning('Vorher i.O., jetzt n.i.O.');
+        disp_array(f_jj', '%1.3e'), disp_array(f2_jj', '%1.3e'), ...
+        disp_array(1e2*test_f2_rel', '%1.2f'));
+      % Versuche erneut mit vorgegebenen Gelenkwinkeln aus den
+      % Detail-Ergebnissen
+      if ~isempty(q0)
+        Structure_jj.q0_traj = q0;
+        f3_jj = cds_fitness(R,Set,Traj,Structure_jj,p_jj,p_desopt_jj);
+      else
+        f3_jj = NaN(size(f2_jj));
+      end
+      test_f3_abs = f_jj - f3_jj;
+      test_f3_rel = test_f3_abs ./ f_jj;
+      rescode = NaN; %#ok<NASGU> % Muss überschrieben werden
+      if all(abs(f_jj) < 1e3) && any(abs(f2_jj) > 1e3) % vorher i.O., beim 2. mal n.i.O.
+        if all(~isnan(f3_jj)) && all(abs(f3_jj) < 1e3)
+          rescode = 2;
+          warning('Vorher i.O., jetzt nur bei Vorgabe von q0 auch i.O., sonst n.i.O.');
+        elseif all(~isnan(f3_jj)) 
+          rescode = 3;
+          warning('Vorher i.O., jetzt mit und ohne q0 n.i.O.');
+        else
+          rescode = 4;
+          warning('Vorher i.O., jetzt n.i.O.');
+        end
+      elseif all(abs(f_jj) >= 1e3) && ~any(isnan(test_f3_rel)) % vorher n.i.O., q0 liegt vor
+        if any(abs(test_f3_rel) > 1e-2) % Fehler 1%
+          rescode = 5;
+          warning('Vorher n.i.O., Auch mit q0 nicht exakt reproduzierbar');
+        else % Kein großer Fehler bei Reproduktion mit q0
+          rescode = 6;
+          warning('Vorher n.i.O., Mit q0 exakt reproduzierbar, ohne nicht reproduzierbar');
+        end
+      else
+        rescode = 1;
+        warning('Wert nicht genau reproduzierbar');
       end
     end
-    % Auswertungs-Tabelle schreiben
-    ReproStatsTab = [ReproStatsTab; {i, RobName, jj, f_jj(1), f2_jj(1), ...
-      max(abs(test_f)), rescode}]; %#ok<AGROW>
+    % Auswertungs-Tabelle für den Roboter schreiben
+    details_available = ~isempty(PSO_Detail_Data);
+    ReproStatsTab_Rob = [ReproStatsTab_Rob; {i, RobName, jj, f_jj(1), f2_jj(1), ...
+      max(abs(test_f2_rel)), max(abs(test_f3_rel)), details_available, rescode}]; %#ok<AGROW>
+    writetable(ReproStatsTab_Rob, fullfile(resdir_opt, sprintf('Rob%d_%s', RobNr, RobName), ...
+      sprintf('Rob%d_%s_reproducability_stats.csv', RobNr, RobName)), 'Delimiter', ';');
+    % In Gesamt-Tabelle anhängen und diese auch schreiben. Dadurch viele
+    % Schreibzugriffe auf die Datei, aber auch bei Abbruch gefüllt.
+    ReproStatsTab = [ReproStatsTab; ReproStatsTab_Rob]; %#ok<AGROW>
     writetable(ReproStatsTab, fullfile(resdir_opt, 'reproducability_stats.csv'), 'Delimiter', ';');
   end
 end
