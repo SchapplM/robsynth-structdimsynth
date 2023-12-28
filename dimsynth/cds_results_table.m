a1781b127f6af0049ec1382624f698a9ec934107
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2023-12-28 18:00:23 +0100

    [cds] Neue Option symmetric_assembly_mode

--- dimsynth/cds_results_table.m
+++ dimsynth/cds_results_table.m
@@ -138,11 +138,13 @@ for i = 1:length(Structures)
   elseif f <= 1e4*1e5, fval_text = 'Traj.-IK Fehler';
   elseif f <= 1e4*2e5, fval_text = 'AR-Hindernis Eckpkt.'; % ab hier aus cds_constraints.m
   elseif f <= 1e4*3e5, fval_text = 'Bauraum-verl. Eckpkt.';
-  elseif f <= 1e4*4e5, fval_text = 'Selbstkoll. Eckpkt.';
+  elseif f <= 1e4*3.9e5, fval_text = 'Selbstkoll. Eckpkt.';
+  elseif f <= 1e4*4e5, fval_text = 'Einbaulage nicht symmetrisch Eckpkt.';
   elseif f <= 1e4*4.25e5, fval_text = 'Schubzylinder Länge Eckpkt. (symm)';
   elseif f <= 1e4*4.5e5, fval_text = 'Schubzylinder Länge Eckpkt.';
   elseif f <= 1e4*4.9e5, fval_text = 'Beinkettenlänge Eckpkt.';
   elseif f <= 1e4*5e5, fval_text = 'Gestelldurchmesser Eckpkt.';
+  elseif f <= 1e4*5.4e5, fval_text = 'Einbaulage nicht symmetrisch Eckpkt.';
   elseif f <= 1e4*5.5e5, fval_text = 'Plattform-Rotation-Grenze Eckpkt.';
   elseif f <= 1e4*6e5, fval_text = 'Gel.-Pos.-Grenze Eckpkt.';
   elseif f <= 1e4*7e5, fval_text = 'Gel.-Pos.-Spannweite Eckpkt.';
