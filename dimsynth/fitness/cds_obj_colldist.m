229792ede83282d5ea4b10882036b0c3a2170c6f
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2023-12-28 21:42:22 +0100

    [cds] Abstandskriterium verbessert. Jetzt ungeprüfter Bereich rund um die Plattform möglich und gleichzeitig Test der Unterarme

--- dimsynth/fitness/cds_obj_colldist.m
+++ dimsynth/fitness/cds_obj_colldist.m
@@ -55,17 +55,55 @@ if isempty(R.collchecks)
   % Es gibt keine Kollisionsprüfungen. Kennzahl ergibt keinen Sinn.
   return
 end
-%% Kollisionsabstände berechnen
-% Ignoriere die Kollisionsabstände für Gelenkpunkte, die außerhalb des
-% Interaktionsraums liegen
+%% Kollisionsabstände berechnen (für Gelenkpunkte innerhalb des Interaktionsraums)
 % Interaktions-Arbeitsraum in das KS 0 rotieren
 collbodies_iaspc = cds_update_interactionspace(R, Set);
-% Benutze Kollisionsprüfungen aus der Roboterklasse
+
+% Nachverarbeitung der Gelenkpunkte. Ziehe die Plattform-Koppelgelenke
+% für die Prüfung weiter in Richtung der Beinkette. Somit können trotzdem
+% die Unterarme gegeneinander geprüft werden, auch wenn Abstände rund um
+% die Plattform ignoriert werden.
+JP_korr = NaN(size(JP));
+platformcheckthreshold = 0.050;
+for i = 1:size(JP,1) % Zeitschritte
+  jp_i = reshape(JP(i,:), 3, size(JP,2)/3);
+  i_jp = 1; % Für PKM-Basis
+  for k = 1:R.NLEG % Indizes: siehe fkine_coll und weiter unten
+    i_jp = i_jp + 1; % Überspringe Beinketten-Basis-KS
+    i_jp = i_jp + R.Leg(k).NJ; % Letztes Gelenk-KS
+    last_point = jp_i(:,i_jp); % Koordinaten des Plattform-Koppelgelenks
+    % Zurückgehen bis die Koordinaten anders sind (mehrwertige Gelenke)
+    for i_jp_delta = 1:3 % bis zu drei Spalten zurückgehen
+      secondlast_point = jp_i(:,i_jp-i_jp_delta);
+      if any(abs(last_point-secondlast_point)>1e-10), break; end
+    end
+    % Verschiebe die letzten Punkte der PKM in Richtung des vorletzten
+    last_point_new = last_point + (secondlast_point-last_point) * ...
+      1.5*platformcheckthreshold/norm(last_point-secondlast_point);
+    jp_i(:,i_jp-i_jp_delta+1:i_jp) = repmat(last_point_new, 1, i_jp_delta);
+  end
+  i_jp = i_jp + 2; % Plattform- und EE-KS der PKM
+  assert(i_jp == size(jp_i,2), 'Indizierung von jp_i ist falsch');
+  JP_korr(i,:) = jp_i(:);
+end
+% Benutze Kollisionsprüfungen aus der Roboterklasse. Die Ersatzkörper sind
+% als Kapseln von einem Gelenkpunkt zum nächsten modelliert.
 [~, colldist, ~, p_coll] = check_collisionset_simplegeom_mex(R.collbodies, R.collchecks, ...
-  JP, struct('collsearch', false));
+  JP_korr, struct('collsearch', false));
+% Debug: Prüfe, ob die geänderten Punkte etwas bewirken (TODO: Noch unklar,
+% warum die Differenz positiv und negativ ist)
+% [~, colldist2, ~, p_coll2] = check_collisionset_simplegeom_mex(R.collbodies, R.collchecks, ...
+%   JP, struct('collsearch', false));
+% test_cd = colldist2 - colldist;
+% test_cd(abs(test_cd)<1e-10) = 0;
+% if any(test_cd(:) > 0)
+%   warning(['Die Kollisionsabstände haben sich verringert, obwohl sich die ' ...
+%     'Körper voneinander entfernt haben. Logik-Fehler.']);
+% end
 
 % Prüfe anhand der Punkte, ob der Kollisionsabstand im kritischen
-% Interaktionsraum liegt: Mittleren Punkt zwischen den beiden Punkten auf
+% Interaktionsraum liegt
+% Mittleren Punkt zwischen den beiden Punkten auf
 % den beteiligten Körpern bestimmen
 p_coll_mean = (p_coll(:,1:3,:) + p_coll(:,4:6,:)) / 2;
 % Setze Prüfung mit den Kollisionspunkten auf. Zu jeder Abstandsprüfung aus
@@ -88,20 +126,30 @@ for i = 1:n_cb_iaspc
   collchecks_iaspc2 = [collchecks_iaspc2; ...
     [n_cb_iaspc+(1:n_cb_robot)', repmat(uint8(i), n_cb_robot, 1)]]; %#ok<AGROW> 
 end
-CP = [zeros(size(p_coll_mean,3),3), NaN(size(p_coll_mean,3), 3*size(p_coll_mean,1))];
-for ll = 1:3
-  tmp1 = reshape(p_coll_mean(:,ll,:), size(p_coll_mean,1), size(p_coll_mean,3))';
-  CP(:,3+ll:3:end) = tmp1;
+coll_iaspc = true(size(p_coll,3), size(p_coll,1));
+for k = 1:2
+  % Punkte in Eingabeformat der Kollisionsprüfung definieren
+  CP = [zeros(size(p_coll,3),3), NaN(size(p_coll,3), 3*size(p_coll,1))];
+  if      k == 1, p_coll_k = p_coll(:,1:3,:);
+  elseif  k == 2, p_coll_k = p_coll(:,4:6,:);
+  else,           p_coll_k = p_coll_mean; % wird nicht geprüft (konvexe Körper: Mitte liegt automatisch drin)
+  end
+  for ll = 1:3
+    tmp1 = reshape(p_coll_k(:,ll,:), size(p_coll_mean,1), size(p_coll_mean,3))';
+    CP(:,3+ll:3:end) = tmp1;
+  end
+  % Zuweisung prüfen (Debuggen Matrixformat)
+%     assert(all(p_coll_k(1,1:3,1) == CP(1,3+(1:3))));
+%     assert(all(p_coll_k(2,1:3,1) == CP(1,3+(4:6))));
+%     assert(all(p_coll_k(1,1:3,2) == CP(2,3+(1:3))));
+  % Eigentliche Prüfung auf Überschneidung
+  CollSet = struct('collsearch', false);
+  coll_iaspc_k = check_collisionset_simplegeom_mex(collbodies_iaspc2, ...
+    collchecks_iaspc2, CP, CollSet);
+  % Alle geprüften Punkte müssen innerhalb des Interaktionsraums liegen.
+  % Daher UND-Verknüpfung.
+  coll_iaspc = coll_iaspc & coll_iaspc_k;
 end
-% Zuweisung prüfen (Debuggen Matrixformat)
-%   assert(all(p_coll(1,1:3,1) == CP(1,3+(1:3))));
-%   assert(all(p_coll(2,1:3,1) == CP(1,3+(4:6))));
-%   assert(all(p_coll(1,1:3,2) == CP(2,3+(1:3))));
-% Eigentliche Prüfung auf Überschneidung
-CollSet = struct('collsearch', false);
-[coll_iaspc, ~] = check_collisionset_simplegeom_mex(collbodies_iaspc2, ...
-  collchecks_iaspc2, CP, CollSet);
-
 % Ignoriere bestimmte Kollisionsabstände, die eher konstruktive Ursachen
 % haben und für MRK-Klemmabstände nicht kritisch sind.
 for i = 1:size(colldist,1) % Zeitschritte
@@ -126,22 +174,22 @@ for i = 1:size(colldist,1) % Zeitschritte
     r_0_C1 = p_coll(j,1:3,i)';
     r_0_C2 = p_coll(j,4:6,i)';
     % Prüfe, ob die Punkte im Interaktionsbereich liegen.
-    % Wenn ja (coll_iaspc=true), dann setze Kollision auf false.
-    coll_k1 = ~coll_iaspc(i,j);
+    % Wenn ja (coll_iaspc=true), dann Punkt verwerfen
+    exclude_point_ij_var1 = ~coll_iaspc(i,j);
     % Prüfe, ob die Punkte in einem Toleranzbereich der Plattform liegen
     % Wenn ja, dann Kollision nicht berücksichtigen
-    if ~coll_k1
+    if ~exclude_point_ij_var1 && false
       for k = 1:R.NLEG+2
-        coll_k2 = norm(r_c_collignore_all(:,k) - (r_0_C1+r_0_C2)/2) < 50e-3;
-        if coll_k2
+        exclude_point_ij_var2 = norm(r_c_collignore_all(:,k) - (r_0_C1+r_0_C2)/2) < platformcheckthreshold;
+        if exclude_point_ij_var2
           break;
         end
       end
     else
-      coll_k2 = false;
+      exclude_point_ij_var2 = false;
     end
     colldist_ijval = colldist(i,j);
-    if coll_k1 || coll_k2
+    if exclude_point_ij_var1 || exclude_point_ij_var2
       colldist(i,j) = NaN; % Deaktiviere den Kollisionsabstand
     end
     % Debug-Bild
@@ -168,8 +216,10 @@ for i = 1:size(colldist,1) % Zeitschritte
       text(r_W_M(1), r_W_M(2), r_W_M(3), sprintf('%1.1fmm', ...
         colldist_ijval*1e3), 'BackgroundColor', 'y');
       % Zeichne den Toleranzbereich ein
-      pts_W_sphere = R.T_W_0 * [r_c_collignore_all(:,k);1];
-      drawSphere([pts_W_sphere(1:3)',50e-3],'FaceColor', 'g', 'FaceAlpha', 0.3);
+      if exclude_point_ij_var2
+        pts_W_sphere = R.T_W_0 * [r_c_collignore_all(:,k);1];
+        drawSphere([pts_W_sphere(1:3)',platformcheckthreshold],'FaceColor', 'g', 'FaceAlpha', 0.3);
+      end
       % Zeichne Interaktionsbereich
       for kk = 1:length(collbodies_iaspc.type)
         if collbodies_iaspc.type(kk) == 10
@@ -187,6 +237,14 @@ for i = 1:size(colldist,1) % Zeitschritte
             'FaceColor', 'm', 'FaceAlpha', 0.1);
         end
       end
+      numberstring = sprintf('i=%d/%d (Traj.), j=%d/%d (Punkt)', i, size(colldist,1), j, size(colldist,2));
+      if exclude_point_ij_var1
+        title(sprintf('%s: Ausschluss weil nicht in Interaktionsraum', numberstring));
+      elseif exclude_point_ij_var2
+        title(sprintf('%s: Ausschluss weil zu nah an Plattform', numberstring));
+      else
+        title(sprintf('%s: Berücksichtige Punkt für Kennzahl', numberstring));
+      end
       drawnow();
     end
   end % for j
@@ -196,15 +254,11 @@ end % for i
 % in cds_dimsynth_robot geprüft. Dort kann aber noch nicht die PKM mit
 % geschlossenen kinematischen Ketten geprüft werden (IK noch nicht gelöst).
 colldist_range = diff(minmax2(colldist')');
-colldist_range(all(isnan(colldist))) = 0; % deaktivierte Prüfungen
 I_norange = abs(colldist_range) < 1e-10;
 if all(I_norange)
-  % Alle Kollisionsprüfungen haben immer den gleichen Abstand. Nehme den
-  % kleinsten dieser Abstände als kritischsten Wert
-  I_notnan = find(all(~isnan(colldist)));
-  [min2colldist,IIcmintmp] = min( colldist(1,I_notnan) );
-  IIcmin = I_notnan(IIcmintmp); IItmin = 1; % Dummy-Werte
-  fval_debugtext = 'Alle Kollisionsabstände gleich. ';
+  % Alle Kollisionsprüfungen haben immer den gleichen Abstand. Nehme den.
+  min2colldist = colldist(1);
+  IIcmin = 1; IItmin = 1; % Dummy-Werte
 else
   % Bestimme minimale Kollisionsabstände für alle Zeitschritte.
   % Berücksichtige nur Kollisionen, die sich auch ändern.
@@ -219,12 +273,26 @@ if min2colldist > 0 % keine Kollision
   f_colldist2 = 1/min2colldist;
   f_colldist_norm = 2/pi*atan(f_colldist2/10); % Normierung auf 0 bis 1; 1m ist 0.06; 0.1m ist 0.5
   fval = 1e2*f_colldist_norm; % Normiert auf 0 bis 1e2. über 1e2 ist reserviert für Kollision
+  fval_debugtext = sprintf('Kollisionsabstand %1.1fmm.', 1e3*min2colldist);
+elseif isnan(min2colldist)
+  % Keine Objekt-Abstände im Interaktionsraum
+  fval = 0; % Bestmöglicher Wert
+  % Bestmöglichen physikalischen Wert generieren: Einmal quer durch größte
+  % Ausdehnung des Robots in alle Dimensionen.
+  robrange = NaN(3,1);
+  for k = 1:3
+    allentries = JP(:,k:3:end);
+    robrange(k) = diff(minmax2(allentries(:)'));
+  end
+  f_colldist = -norm(robrange);
+  fval_debugtext = sprintf('Keine Kollisionsabstände im Interaktionsraum');
 else % Kollision
   f_colldist2 = -min2colldist; % je größer der Betrag desto tiefer in Kollision
   f_colldist_norm = 2/pi*atan(f_colldist2*10); % 0.2m Eindringung entspricht 0.7; 0.1m entspricht 0.5; 1mm entspricht 0.0064
   fval = 1e2*(1+9*f_colldist_norm); % normiere auf 1e2 bis 1e3
+  fval_debugtext = sprintf('Kollisionsdurchdringung %1.1fmm.', -1e3*min2colldist);
 end
-fval_debugtext = [fval_debugtext, sprintf('Kollisionsabstand %1.1fmm.', 1e3*min2colldist)];
+
 if min2colldist < 0
   fval_debugtext = [fval_debugtext, ' (Kollision)'];
 end
@@ -257,13 +325,14 @@ end
 % Set_tmp.general.plot_details_in_fitness = inf;
 % [fval, coll, colldepth_abs] = cds_constr_collisions_self(R, Traj_0.X, ...
 %   Set_tmp, Structure, JP, Q, [1 2]);
-%% Debug-Plot
+%% Prüfe Aktivierung des Debug-Plots
 if Set.general.plot_details_in_fitness < 0 && 1e4*fval > abs(Set.general.plot_details_in_fitness) || ... % Gütefunktion ist schlechter als Schwellwert: Zeichne
    Set.general.plot_details_in_fitness > 0 && 1e4*fval < abs(Set.general.plot_details_in_fitness) % Gütefunktion ist besser als Schwellwert: Zeichne
   % Zeichnen/Debuggen (s.u.)
 else
   return
 end
+%% Debug-Plot anzeigen
 fhdl = change_current_figure(722); clf; hold all;
 view(3); axis auto; hold on; grid on;
 xlabel('x in m'); ylabel('y in m'); zlabel('z in m');
