ec6e0800144a7c02ce10e3ab7269699c2e3aff1c
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2023-12-30 10:02:08 +0100

    [cds] Neues MRK-Kriterium basierend auf effektiver Masse der Plattform

--- dimsynth/misc/cds_definitions.m
+++ dimsynth/misc/cds_definitions.m
@@ -13,11 +13,11 @@ function defstruct = cds_definitions()
 defstruct = struct();
 % Nebenbedingungen der Optimierung
 defstruct.objconstr_names_all = {'mass', 'energy', 'actforce', 'condition', ...
-  'stiffness', 'materialstress', 'positionerror', 'mrk1', 'mrk2', 'mrk3'};
+  'stiffness', 'materialstress', 'positionerror', 'mrk1', 'mrk2', 'mrk3', 'mrk4'};
 % Zielkriterien der Optimierung (für Ausgabe aller Zielkriterien in Tabelle)
 defstruct.obj_names_all = {'mass', 'energy', 'power', 'actforce', 'materialstress', 'condition', ...
   'manipulability', 'minjacsingval', 'positionerror', 'jointrange', 'jointlimit', ...
-  'actvelo','chainlength', 'installspace', 'footprint', 'colldist', 'mrk1', 'mrk2', 'mrk3', 'stiffness'}; % konsistent zu fval_obj_all und physval_obj_all
+  'actvelo','chainlength', 'installspace', 'footprint', 'colldist', 'mrk1', 'mrk2', 'mrk3', 'mrk4', 'stiffness'}; % konsistent zu fval_obj_all und physval_obj_all
 % Zielkriterien der inversen Kinematik
 defstruct.objective_ik_names_all = {'default', 'ikjac_cond', 'jac_cond', ...
   'coll_par', 'instspc_par', 'poserr_ee', 'none', 'constant', 'maxactvelo', 'maxactforce'};
