486dafca80c28d2fb12a778b3aa6c56e7caaaa3c
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2024-01-05 18:20:54 +0100

    [cds] Korrektur zu vorherigem Commit

--- dimsynth/misc/cds_paretoplot_createfcn.m
+++ dimsynth/misc/cds_paretoplot_createfcn.m
@@ -9,8 +9,8 @@
 % OptName
 %   Name der Optimierung
 % oldcreatefcn
-%   Ursprüngliche CreateFcn des Figures. Notwendig für unsichtbares
-%   Speichern des Bildes und Sichtbarmachen beim Neu-Öffnen
+%   Ursprüngliche CreateFcn des Figures als Char-String. Notwendig für
+%   unsichtbares Speichern des Bildes und Sichtbarmachen beim Neu-Öffnen
 % 
 % Beispiele:
 % Zum manuellen Eintragen der CreateFcn oder bei Änderung des Namens der
@@ -24,8 +24,12 @@
 % (C) Institut für Mechatronische Systeme, Leibniz Universität Hannover
 
 function cds_paretoplot_createfcn(fighdl, dummy, OptName, oldcreatefcn)
-if nargin == 4
-  eval(oldcreatefcn);
+if nargin == 4 && isa(oldcreatefcn, 'char')
+  try
+    eval(oldcreatefcn);
+  catch err
+    warning('Keine Ausführung der CreateFcn möglich: %s\n %s', oldcreatefcn, err.message);
+  end
 end
 %% Trage die ButtonDownFcns ein
 fch = get(fighdl, 'Children');
