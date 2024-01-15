3df40b557c1932dbc791e46f432c877df2ba09f8
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2024-01-15 13:39:31 +0100

    [cds] Fehlerkorrektur

--- dimsynth/misc/cds_log.m
+++ dimsynth/misc/cds_log.m
@@ -71,7 +71,7 @@ if nargin > 2 && any(strcmp(option, {'init', 'amend'}))
     logdir = fileparts(logfilepath);
     movefile(logfilepath, fullfile(logdir, backupfilename));
     % Log-Datei komprimieren (verbleibt sonst unkomprimiert im Ordner)
-    gzip(backupfilename); delete(backupfilename);
+    gzip(fullfile(logdir, backupfilename)); delete(fullfile(logdir, backupfilename));
   end
 end
 % Falls Fitness-Funktion nachträglich aufgerufen wird, ist das Loggen nicht
