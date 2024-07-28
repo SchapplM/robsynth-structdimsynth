227ab036cc40d145c238e043295955130272f557
Moritz Schappler <moritz.schappler@imes.uni-hannover.de>
2024-07-28 21:26:31 +0200

    [cds] Fehlerkorrektur (falls keine Generationen-Sicherungsdateien vorhanden sind)

--- dimsynth/cds_gen_init_pop.m
+++ dimsynth/cds_gen_init_pop.m
@@ -87,6 +87,7 @@ for i = 1:length(tmpdirsrob)
     genfiles_numbers(j) = str2double(ttt{1}{1});
   end
   [~,genfiles_order] = sort(genfiles_numbers, 'ascend');
+  igen = 0;
   for j = genfiles_order(:)' % Dateien durchgehen. Neue überschreibt alte.
     try % Auf Cluster Dateisystem-Fehler möglich
       tmp = load(fullfile(genfiles(j).folder, genfiles(j).name));
